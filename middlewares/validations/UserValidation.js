9b3ed4d31d7ed75c59a3b73a0dd7b4e5839a8980
Oyomi <seayomi@gmail.com>
2019-08-09 16:02:19 +0100

    ft(send-sms): send messages - create message tables - create message controller - implement sending  message process

--- middlewares/validations/UserValidation.js
+++ middlewares/validations/UserValidation.js
@@ -39,6 +39,23 @@ class UserValidation {
     });
   }
 
+  static sendValidation(req, res, next) {
+    const msgInfo = {
+      phone: "required|digits:11",
+      message: "required"
+    };
+    const validator = new Validator(req.body, msgInfo);
+    validator.passes(() => next());
+    validator.fails(() => {
+      const errors = validator.errors.all();
+      return res.status(400).json({
+        status: "error",
+        statusCode: 400,
+        errors
+      });
+    });
+  }
+
 }
 
 export default UserValidation;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import authRouter from './auth';
+import messageRouter from './message';
 
 const apiRoutes = Router();
 
 // API Routes
 apiRoutes.use('/auth', authRouter);
+apiRoutes.use('/message', messageRouter);
 
 // Matches /api the API home route
 apiRoutes.get('/', (req, res) => {
